49195fc0bc03a6ad66dc936b045b354db19ac6e8
Mattia Visini <mattiavisini95@gmail.com>
2021-04-27 10:16:39 +0200

    Creato classe LeggiXMl per leggere dai file inputPersone e comuni

--- src/it/unibs/ing/fp/codicifiscali/Main.java
+++ src/it/unibs/ing/fp/codicifiscali/Main.java
@@ -1,12 +1,23 @@
 package it.unibs.ing.fp.codicifiscali;
 
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static final String FILENAME_COMUNI = "comuni.xml";
+    public static final String FILENAME_PERSONE = "inputPersone.xml";
 
-        ArrayList<Persona> persone = new ArrayList<>();
+    public static void main(String[] args) {
+        //letture da file dei comuni
         ArrayList<Comune>  comuni = new ArrayList<>();
+        DocumentBuilderFactory comune_builder = DocumentBuilderFactory.newInstance();
+        LeggiXML.extractedCity(comuni, comune_builder,FILENAME_COMUNI);
+        
+        //lettura da file delle persone
+        ArrayList<Persona> persone = new ArrayList<>();
+        DocumentBuilderFactory people_builder = DocumentBuilderFactory.newInstance();
+        LeggiXML.extractedPersone(persone, people_builder,FILENAME_PERSONE,comuni);
+
 
     }
 
