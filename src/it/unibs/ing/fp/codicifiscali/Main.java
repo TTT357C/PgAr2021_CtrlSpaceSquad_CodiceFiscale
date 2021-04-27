e1c65932cd3b20b4dd344dcfb9ef21be0bc37da5
Thomas <thomas.causetti@gmail.com>
2021-04-27 15:48:13 +0200

    Menu

--- src/it/unibs/ing/fp/codicifiscali/Main.java
+++ src/it/unibs/ing/fp/codicifiscali/Main.java
@@ -1,27 +1,62 @@
 package it.unibs.ing.fp.codicifiscali;
 
+import it.unibs.ing.fp.mylib.InputDati;
+
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.util.ArrayList;
 
 public class Main {
     public static final String FILENAME_COMUNI = "comuni.xml";
     public static final String FILENAME_PERSONE = "inputPersone.xml";
+    public static final String MENU = "  ___________________________________\n"
+                                    + " |                                   |\n"
+                                    + " |   - Codici Fiscali -              |\n"
+                                    + " |                                   |\n"
+                                    + " |   1 - Inizia Programma            |\n"
+                                    + " |   0 - Exit                        |\n"
+                                    + " |                                   |\n"
+                                    + " |___________________________________|\n";
 
     public static void main(String[] args) {
-        //letture da file dei comuni
-        ArrayList<Comune>  comuni = new ArrayList<>();
-        DocumentBuilderFactory comune_builder = DocumentBuilderFactory.newInstance();
-        LeggiXML.extractedCity(comuni, comune_builder,FILENAME_COMUNI);
-        
-        //lettura da file delle persone
-        ArrayList<Persona> persone = new ArrayList<>();
-        DocumentBuilderFactory people_builder = DocumentBuilderFactory.newInstance();
-        LeggiXML.extractedPersone(persone, people_builder,FILENAME_PERSONE,comuni);
+
+        int scelta=0;
+        do{
+            System.out.println(MENU);
+            scelta = InputDati.leggiIntero(" -->", 0, 1);
+
+            //Nel caso avvenga un eccezione il programma visualizza un messaggio di errore ma non termina l'esecuzione
+            try {
+                switch (scelta) {
+                    case 0:
+                        System.out.println("\n _ Arrivederci _____________ \n");
+                        break;
+                    case 1:
+                        //letture da file dei comuni
+                        ArrayList<Comune>  comuni = new ArrayList<>();
+                        DocumentBuilderFactory comune_builder = DocumentBuilderFactory.newInstance();
+                        LeggiXML.extractedCity(comuni, comune_builder,FILENAME_COMUNI);
+
+                        //lettura da file delle persone
+                        ArrayList<Persona> persone = new ArrayList<>();
+                        DocumentBuilderFactory people_builder = DocumentBuilderFactory.newInstance();
+                        LeggiXML.extractedPersone(persone, people_builder,FILENAME_PERSONE,comuni);
+
+                        System.out.println(comuni);
+                        System.out.println(persone);
+                        break;
+                    default:
+                        break;
+                }
+            }catch (Exception e){
+                System.err.println(" Errore Ritorno a Menu ");
+                System.err.println(" ("+e+")");
+                System.out.println("\n");
+            }
+
+        }while(scelta!=0);
 
 
 
-        System.out.println(comuni);
-        System.out.println(persone);
     }
 
 
