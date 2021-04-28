e487d1a3db26ed13e29b927ff226b25a58eac10a
Mattia Visini <mattiavisini95@gmail.com>
2021-04-28 23:42:35 +0200

    Modifica validitaCodiceFiscale

--- src/it/unibs/ing/fp/codicifiscali/CodiceFiscale.java
+++ src/it/unibs/ing/fp/codicifiscali/CodiceFiscale.java
@@ -6,15 +6,28 @@ import java.util.Objects;
  * @author Thomas Causetti
  */
 public class CodiceFiscale {
-    private String codice_fiscale;
 
+    public static final int ASCII_ZERO = 48;
+    public static final int ASCII_NOVE = 57;
+    public static final int ASCII_A = 65;
+    public static final int ASCII_Z = 90;
     public static final char[] ARR_VOCALI = {'A', 'E', 'I', 'O', 'U'};
     public static final char[] ARR_CONSONANTI = {'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'};
-    static String [] ARR_MESI_IN_LETTERE = {"A","B","C","D","E","H","L","M","P","R","S","T"};
-    static int [] ARR_VALORI_CHAR_PARI = {0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
-    static int [] ARR_VALORI_CHAR_DISPARI = {1,0,5,7,9,13,15,17,19,21,1,0,5,7,9,13,15,17,19,21,2,4,18,20,11,3,6,8,12,14,16,10,22,25,24,23};
-    static int [] ARR_GIORNI_IN_UN__MESE = {31,28,31,30,31,30,31,31,30,31,30,31};
+    public static final int INCREMENTO_MESE_DONNE = 40;
+    public static final int DELTA_ARRAY_SOMMA_CHAR = 55;
+    public static final int ALFABETO_LENGTH = 26;
+    static final char [] ARR_MESI_IN_LETTERE = {'A','B','C','D','E','H','L','M','P','R','S','T'};
+    public static final int CF_LENGTH = 16;
+    static final int [] ARR_VALORI_CHAR_PARI = {0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
+    static final int [] ARR_VALORI_CHAR_DISPARI = {1,0,5,7,9,13,15,17,19,21,1,0,5,7,9,13,15,17,19,21,2,4,18,20,11,3,6,8,12,14,16,10,22,25,24,23};
+    static final int [] ARR_GIORNI_IN_UN__MESE = {31,28,31,30,31,30,31,31,30,31,30,31};
 
+    private String codice_fiscale;
+
+    /**
+     * Costruttore di codice fiscale
+     * @param codice_fiscale
+     */
     public CodiceFiscale(String codice_fiscale) {
         this.codice_fiscale = codice_fiscale;
     }
@@ -23,7 +36,6 @@ public class CodiceFiscale {
      * Metodo che ritorna codice fiscale
      * @return Ritorna codice fiscale
      */
-
     public String getCodice_fiscale() {
         return codice_fiscale;
     }
@@ -175,7 +187,8 @@ public class CodiceFiscale {
             return false;
         }
     }
-    //TODO ESTRARRE MAGIC NUMBER
+
+
     /**
      * Metodo che controlla la validità di un codice fiscale
      * @return Ritorna True se il codice fiscale è valido, false altrimenti
@@ -185,155 +198,51 @@ public class CodiceFiscale {
 
         String cf = (this.getCodice_fiscale()).toUpperCase();
         //controllo lunghezza uguale a 16 caratteri altrimenti ritorna subito false
-        if(cf.length()==16) {
+        if(cf.length() == CF_LENGTH) {
             String ctrl_surname = cf.substring(0,3);
-            //CONTROLLLO 3 cognome
-            boolean seq_sur [] = {false,false,false};
-            boolean checksur=false;
-            for(int i=0; i < ARR_CONSONANTI.length ;i++ ) {
-                for (int j = 0; j < ctrl_surname.length(); j++) {
-                    if((ctrl_surname.substring(j,j+1)).equals(ARR_CONSONANTI[i])) {
-                        seq_sur[j] = true;
-                    }
-                }
-            }
-            if(areAllTrue(seq_sur)) {
-                //Tutte consonanti
-                checksur=true;
-            }
-            else if(seq_sur[1]==false && seq_sur[2]==true){
-                if(isVocale(ctrl_surname.subSequence(1,2)) && ctrl_surname.substring(2,3).equalsIgnoreCase("X")) {
-                    checksur=true;
-                }
-            }
-            else if(seq_sur[2]==false && seq_sur[1]==true && seq_sur[0]==true){
-                checksur=true;
-            }
-            else if(seq_sur[2]==false && seq_sur[1]==false && seq_sur[0]==true) {
-                checksur=true;
-            }//FINE CONTROLLO LETTERE COGNOME
+            //CONTROLLO COGNOME
+            boolean checksur = checkSurname(ctrl_surname);
             if(checksur==false){
                 return false;
             }
-
             //INIZIO CONTROLLO NOME
-            String ctrl_name = cf.substring(3,6);
-            boolean seq_name [] = {false,false,false};
-            boolean check_name=false;
-            for(int i=0; i < ARR_CONSONANTI.length ;i++ ) {
-                for (int j = 0; j < ctrl_name.length(); j++) {
-                    if((ctrl_name.substring(j,j+1)).equals(ARR_CONSONANTI[i])) {
-                        seq_name[j] = true;
-                    }
-                }
-            }
-            if(areAllTrue(seq_name)) {//Tutte consonanti
-                check_name=true;
-            }
-            else if(seq_name[1]==false && seq_name[2]==true){ //CASO FOX NOME DI 2 LETTERE
-                if(isVocale(ctrl_name.subSequence(1,2)) && ctrl_name.substring(2,3).equalsIgnoreCase("X")) {
-                    check_name=true;
-                }
-            }
-            else if(seq_name[2]==false && seq_name[1]==true && seq_name[0]==true){ //EX NOME TRE
-                check_name=true;
-            }
-            else if(seq_name[2]==false && seq_name[1]==false && seq_name[0]==true) { //NOME £ LETTERE CON 2 VOCALI EX. ADA
-                check_name=true;
-            }//FINE CONTROOLO NOME
-            if(check_name==false) {
+            boolean check_name = checkName(cf);
+            if(check_name == false) {
                 return false;
             }
-            
             //CONTROLLO 2 cifre anno
-            String ctrlyear = cf.substring(6,8);
-            boolean check_years=false;
-            if(ctrlyear.charAt(0)>=48 && ctrlyear.charAt(0)<=57 && ctrlyear.charAt(1)>=48 && ctrlyear.charAt(1)<=57) {
-                check_years=true;
-            } //FINE CHECK YEAR
-           if(check_years==false){
+            boolean check_years = checkYear(cf);
+            if(check_years==false){
                return false;
            }
-
             //CHECK MESE
-            String ctrlmonth = cf.substring(8,9);
+            char ctrlmonth = cf.charAt(8);
             boolean check_month =false;
             int pos_mese=0;
             for (int i = 0; i < ARR_MESI_IN_LETTERE.length; i++) {
-                if(ctrlmonth.equalsIgnoreCase(ARR_MESI_IN_LETTERE[i])) {
-                    check_month =true;
-                    pos_mese=i;
+                if(ctrlmonth == (ARR_MESI_IN_LETTERE[i])) {
+                    check_month = true;
+                    pos_mese = i;
                 }
             }//FINE CHECK MESE
             if(check_month==false){
                 return false;
             }
-
             //CHECK GIORNO
-            String ctrl_day = cf.substring(9,11);
-            boolean check_day=false;
-            if(ctrl_day.charAt(0)>= 48 && ctrl_day.charAt(0)<=57 && ctrl_day.charAt(1)>=48 && ctrl_day.charAt(1)<=57) {
-                int ctrldayInt = Integer.parseInt(ctrl_day);
-                if(ctrldayInt > 0 && ctrldayInt <= ARR_GIORNI_IN_UN__MESE[pos_mese]) {
-                    check_day = true;
-                }
-            }//FINE CHECK GIORNO
+            boolean check_day = checkDay(cf, pos_mese);
             if(check_day==false){
                 return false;
             }
-
             //check codice paese controllo che sia presente in comuni
-            String ctrl_code = cf.substring(11,14);
-            boolean checkcode=false;
-            //Controllo in arraylist Comuni Italiani
-            for (int i = 0; i < Main.getComuni().size() ; i++) {
-                if (ctrl_code.equalsIgnoreCase(Main.getComuni().get(i).getCodice())){
-                    checkcode=true;
-                }
-            }
-            //per straniero
-            if(ctrl_code.substring(0,1).equals("Z")) {
-                if(ctrl_code.charAt(1)>=48 && ctrl_code.charAt(1)<=57
-                        && ctrl_code.charAt(2)>=48 && ctrl_code.charAt(2)<=57
-                        && ctrl_code.charAt(3)>=48 && ctrl_code.charAt(3)<=57) {
-                    checkcode=true;
-                }
-            } //fine check codice nata all'estero
+            boolean checkcode = checkCode(cf);
             if(checkcode==false){
                 return false;
             }
-
             //CONTROLLO carattere di controllo
             int sum=0;
-            for (int i = 0; i < (cf.length()-1); i++) {
-                if(i%2==0) {
-                    //caratteri in posizione pari
-                    char char_temp = cf.charAt(i);
-                    if(char_temp>=48 && char_temp<=57 ) {
-                        //se è un numero in posizione pari
-                        sum=sum+ ARR_VALORI_CHAR_PARI[char_temp-48];
-                    }else {
-                        //se è lettera in posizione pari
-                        if(char_temp>=65 && char_temp<=90) {
-                            sum=sum+ ARR_VALORI_CHAR_PARI[char_temp-55];
-                        }
-                    }
-                }else {
-                    //caratteri in posizione dispari
-                    char t = cf.charAt(i);
-                    if(t>=48 && t<=57 ) {
-                        //se è un numero in posizione dispari
-                        sum=sum+ARR_VALORI_CHAR_DISPARI[t-48];
-                    }else {
-                        //se è una lettera in posizione dispari
-                        if(t>=65 && t<=90) {
-                            sum=sum+ARR_VALORI_CHAR_DISPARI[t-55];
-                        }
-                    }
-                }
-            }
-            char codice_teorico=(char)((sum%26)+65);
-            char codice_reale=cf.charAt(15);
+            sum = sommaChar(cf, sum);
+            char codice_teorico = (char)((sum % ALFABETO_LENGTH) + ASCII_A);
+            char codice_reale = cf.charAt(CF_LENGTH-1);
             if(codice_teorico == codice_reale) {
                 return true;
             }
@@ -346,15 +255,176 @@ public class CodiceFiscale {
         }
     }
 
+    private boolean checkCode(String cf) {
+        String ctrl_code = cf.substring(11,15);
+        boolean checkcode=false;
+        //Controllo in arraylist Comuni Italiani
+        checkcode = isCheckItalyCode(ctrl_code, checkcode);
+        //Per straniero
+        checkcode = isCheckForeignCode(ctrl_code, checkcode);
+        return checkcode;
+    }
+
+    private boolean checkDay(String cf, int pos_mese) {
+        String ctrl_day = cf.substring(9,11);
+        boolean check_day=false;
+        check_day = isCheckDay(pos_mese, ctrl_day, check_day);
+        return check_day;
+    }
+
+    private boolean checkYear(String cf) {
+        String ctrlyear = cf.substring(6,8);
+        boolean check_years = isCheckYears(ctrlyear);
+        return check_years;
+    }
+
+    private boolean checkName(String cf) {
+        String ctrl_name = cf.substring(3,6);
+        boolean seq_name [] =sonoConsonanti(ctrl_name);
+        boolean check_name = false;
+        check_name = isChecksur(ctrl_name, seq_name);
+        return check_name;
+    }
+
+    private boolean checkSurname(String ctrl_surname) {
+        boolean checksur=false;
+        boolean seq_sur [] =sonoConsonanti(ctrl_surname);
+        checksur = isChecksur(ctrl_surname, seq_sur);
+        return checksur;
+    }
+
+    private int sommaChar(String cf, int sum) {
+        for (int i = 0; i < (cf.length()-1); i++) {
+            if((i+1)%2 == 0) {
+                //caratteri in posizione pari
+                char char_temp = cf.charAt(i);
+                if(char_temp >= ASCII_ZERO && char_temp <= ASCII_NOVE ) {
+                    //se è un numero in posizione pari
+                    sum = sum + ARR_VALORI_CHAR_PARI[char_temp-ASCII_ZERO];
+                }else {
+                    //se è lettera in posizione pari
+                    if(char_temp>=ASCII_A && char_temp<=ASCII_Z) {
+                        sum = sum + ARR_VALORI_CHAR_PARI[char_temp- DELTA_ARRAY_SOMMA_CHAR];
+                    }
+                }
+            }else {
+                //caratteri in posizione dispari
+                char t = cf.charAt(i);
+                if(t >= ASCII_ZERO && t <= ASCII_NOVE ) {
+                    //se è un numero in posizione dispari
+                    sum = sum + ARR_VALORI_CHAR_DISPARI[t-ASCII_ZERO];
+                }else {
+                    //se è una lettera in posizione dispari
+                    if(t >= ASCII_A && t <= ASCII_Z) {
+                        sum = sum + ARR_VALORI_CHAR_DISPARI[t-DELTA_ARRAY_SOMMA_CHAR];
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+
+    private boolean isCheckForeignCode(String ctrl_code, boolean checkcode) {
+        if(ctrl_code.charAt(0) == 'Z') {
+            if(ctrl_code.charAt(1)>=ASCII_ZERO && ctrl_code.charAt(1)<=ASCII_NOVE
+                    && ctrl_code.charAt(2)>=ASCII_ZERO && ctrl_code.charAt(2)<=ASCII_NOVE
+                    && ctrl_code.charAt(3)>=ASCII_ZERO && ctrl_code.charAt(3)<=ASCII_NOVE) {
+                checkcode =true;
+            }
+        } //fine check codice nata all'estero
+        return checkcode;
+    }
+
+    private boolean isCheckItalyCode(String ctrl_code, boolean checkcode) {
+         checkcode = false;
+        for (int i = 0; i < Main.getComuni().size() ; i++) {
+            if (ctrl_code.equals(Main.getComuni().get(i).getCodice())){
+                checkcode =true;
+            }
+        }
+        return checkcode;
+    }
+
+    /**
+     * Calcola se il giorno rappresentato sul c.f. è valido
+     * @param pos_mese Posizione del mese nell'array calcolato precedentemente
+     * @param ctrl_day stringa rappresentante un numero
+     * @param check_day
+     * @return Ritorna true se il giorno di nascita è valido false altrimenti
+     */
+    private boolean isCheckDay(int pos_mese, String ctrl_day, boolean check_day) {
+        check_day =false;
+        if(ctrl_day.charAt(0) >= ASCII_ZERO && ctrl_day.charAt(0) <= ASCII_NOVE && ctrl_day.charAt(1) >= ASCII_ZERO && ctrl_day.charAt(1) <= ASCII_NOVE) {
+            int ctrldayInt = Integer.parseInt(ctrl_day);
+            if(ctrldayInt > 0 && (ctrldayInt <= ARR_GIORNI_IN_UN__MESE[pos_mese] ||  (ctrldayInt <= (ARR_GIORNI_IN_UN__MESE[pos_mese]+ INCREMENTO_MESE_DONNE)))) {
+                check_day = true;
+            }
+        }//FINE CHECK GIORNO
+        return check_day;
+    }
+
+    /**
+     * Metodo chhe controlla che sia inserito un numero rappresentato da 2 cifre
+     * @param ctrlyear Stringa di 2 caratteri rappresentanti l'anno di nascita
+     * @return True se è un anno valido false altrimenti
+     */
+    private boolean isCheckYears(String ctrlyear) {
+        boolean check_years=false;
+        if(ctrlyear.charAt(0) >= ASCII_ZERO && ctrlyear.charAt(0) <= ASCII_NOVE && ctrlyear.charAt(1) >= ASCII_ZERO && ctrlyear.charAt(1) <= ASCII_NOVE) {
+            check_years =true;
+        } //FINE CHECK YEAR
+        return check_years;
+    }
+
+    /**
+     * Calcola se la disposizione delle prime tre lettere del c.f. sono in un formato valido
+     * @param ctrl_surname Prime tre lettere del codice fiscale
+     * @param seq_sur Calcolato dal metodo sonoConsonanti
+     * @return Boolean true se il formato è corretto con controllo anche sul nome, false altrimenti
+     */
+    private boolean isChecksur(String ctrl_surname, boolean[] seq_sur) {
+        boolean checksur =false;
+        if (areAllTrue(seq_sur)) {
+            //Tutte consonanti
+            checksur = true;
+        } else if (seq_sur[1] == false && seq_sur[2] == true) {
+            if (isVocale(ctrl_surname.charAt(1)) && ctrl_surname.charAt(2) == 'X') {
+                checksur = true;
+            }
+        } else if (seq_sur[2] == false && seq_sur[1] == true && seq_sur[0] == true) {
+            checksur = true;
+        } else if (seq_sur[2] == false && seq_sur[1] == false && seq_sur[0] == true) {
+            checksur = true;
+        }//FINE CONTROLLO LETTERE COGNOME
+        return checksur;
+    }
+
+    /**
+     * Data le prime tre lettere del cognome trova se è consonante true false altrimenti
+     * @param ctrl_surname
+     * @return Array di boolean true se in posizione una consonante
+     */
+    private boolean[] sonoConsonanti(String ctrl_surname) {
+        boolean [] seq_nick={false,false,false};
+        for (int i = 0; i < ARR_CONSONANTI.length; i++) {
+            for (int j = 0; j < ctrl_surname.length(); j++) {
+                if ((ctrl_surname.charAt(j)) == (ARR_CONSONANTI[i])) {
+                    seq_nick[j] = true;
+                }
+            }
+        }
+        return seq_nick;
+    }
+
     /**
      *
      * @param charSequence Carattere tipo char
      * @return Ritorna true se è una vocale false altrimenti
      */
-    private static boolean isVocale(CharSequence charSequence) {
+    private static boolean isVocale(char charSequence) {
         // TODO Auto-generated method stub
         for (int i = 0; i < ARR_VOCALI.length; i++) {
-            if(((String) charSequence).equals(ARR_VOCALI[i])) {
+            if( charSequence == ARR_VOCALI[i]) {
                 return true;
             }
         }
